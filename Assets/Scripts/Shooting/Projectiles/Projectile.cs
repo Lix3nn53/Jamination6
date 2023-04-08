b5d83e0c8e90f727b12c518672274342c417607a
Lix3nn53 <lixakyildiz@gmail.com>
2023-04-08 17:04:59 +0300

    More work on projectiles

--- Assets/Scripts/Shooting/Projectiles/Projectile.cs
+++ Assets/Scripts/Shooting/Projectiles/Projectile.cs
@@ -6,8 +6,19 @@ public class Projectile : MonoBehaviour
 {
   [SerializeField] private float _life = 5;
 
+  [SerializeField] private GameObject _spawnOnHitPrefab;
+
   void Start()
   {
     Destroy(gameObject, _life);
+  }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (_spawnOnHitPrefab != null)
+    {
+      Instantiate(_spawnOnHitPrefab, transform.position, transform.rotation);
+    }
+    Destroy(gameObject);
   }
 }
